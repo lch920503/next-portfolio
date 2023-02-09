c7fedb7044c4fb1be870c28874fbbe3eb3840dcf
lch09253 <lch09253@gmail.com>
2023-02-09 11:26:27 +0900

    Add images and next/image domains

--- src/components/projects/ProductItem.js
+++ src/components/projects/ProductItem.js
@@ -5,19 +5,25 @@ export default function ProductItem({ data }) {
   const tags = data.properties.태그.multi_select;
   const description = data.properties.이름.title[0].text.content;
   const imgSrc = data.cover.file?.url || data.cover.external.url;
+  const url = data.properties.URL.url;
+  console.log(url);
 
   return (
     <li className="project-card mb-3 rounded-xl overflow-hidden">
       <Image
-        className="rounded-t-xl w-full max-h-80 object-cover"
+        className="rounded-t-xl w-full h-60 object-cover"
         src={imgSrc}
-        width={100}
-        height={100}
+        width="500"
+        height="500"
         alt="Cover Image"
+        quality={100}
       />
       <div className="p-4">
         <strong>{title}</strong>
         <p>{description}</p>
+        <span>
+          배포주소 <a href={url}>{url}</a>
+        </span>
         <span className="tags flex flex-wrap">
           {tags.map((tag) => (
             <span
