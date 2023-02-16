6e84e3115bd6c944593c97755e5472f773a23ebe
lch09253 <lch09253@gmail.com>
2023-02-16 15:48:49 +0900

    Change CSS darkmode

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,21 +1,21 @@
 export default function Contact() {
   return (
-    <section className="text-gray-400 bg-gray-900 body-font">
+    <section className="text-gray-600 body-font">
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-wrap -m-4">
           <div className="p-4 w-full">
-            <div className="h-full bg-gray-800 bg-opacity-40 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center">
-              <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
+            <div className="h-full bg-gray-100 dark:bg-gray-700 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+              <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
                 Contact
               </h2>
-              <h1 className="title-font sm:text-2xl text-xl font-medium text-white mb-3">
+              <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
                 이채현
               </h1>
-              <p className="mb-3">010-3914-6929</p>
-              <p className="mb-3">
+              <p className="text-gray-400 mb-3">010-3914-6929</p>
+              <p className="text-gray-400 mb-3">
                 lch09253@gmail.com <br /> lch09253@daum.net
               </p>
-              <a className="text-indigo-400 inline-flex items-center">
+              <a className="text-indigo-500 inline-flex items-center github">
                 https://github.com/lch920503/
               </a>
             </div>
