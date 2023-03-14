783dec58994664d8d7a4660e043b646c47f312e9
lch09253 <lch09253@gmail.com>
2023-03-14 20:09:14 +0900

    Change contents

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -7,14 +7,14 @@ export default function ProjectList({ data }) {
         총 프로젝트: {data.results.length}개
       </h2>
       <p className="mt-4 text-xl font-bold sm:text-2xl">
-        (프론트 개발 4개, 퍼블리싱 4개)
+        (프론트 개발 4개, 퍼블리싱 5개)
       </p>
       <span className="block mt-4 dark:text-white px-2 break-keep text-center">
         아래의 프로젝트 모두 <strong className="underline">참여도 100%</strong>
         의 작업물입니다.
       </span>
       <strong className="mt-4 text-center break-keep dark:text-white">
-        Next.js 1개, React.js 3개, Javascript 3개, jQuery 1개
+        Next.js 1개, React.js 3개, Javascript 4개, jQuery 1개
       </strong>
       <ul className="max-w-screen-sm sm:max-w-screen-lg lg:w-3/4 grid grid-cols-1 gap-8 p-8 m-4 lg:grid-cols-2">
         {data.results.map((project) => (
