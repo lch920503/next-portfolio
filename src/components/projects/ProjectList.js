884192f4a19026b6e8e64d91b2b2052c0b0808e5
lch09253 <lch09253@gmail.com>
2023-03-14 20:30:19 +0900

    Change Style

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -2,13 +2,10 @@ import ProjectItem from "./ProjectItem";
 
 export default function ProjectList({ data }) {
   return (
-    <section className="flex flex-col items-center justify-center min-h-screen pt-4">
+    <section className="flex flex-col items-center justify-center min-h-screen">
       <h2 className="text-4xl font-bold sm:text-6xl">
         총 프로젝트: {data.results.length}개
       </h2>
-      <p className="mt-4 text-xl font-bold sm:text-2xl">
-        (프론트 개발 4개, 퍼블리싱 5개)
-      </p>
       <span className="block mt-4 dark:text-white px-2 break-keep text-center">
         아래의 프로젝트 모두 <strong className="underline">참여도 100%</strong>
         의 작업물입니다.
@@ -16,8 +13,15 @@ export default function ProjectList({ data }) {
       <strong className="mt-4 text-center break-keep dark:text-white">
         Next.js 1개, React.js 3개, Javascript 4개, jQuery 1개
       </strong>
-      <ul className="max-w-screen-sm sm:max-w-screen-lg lg:w-3/4 grid grid-cols-1 gap-8 p-8 m-4 lg:grid-cols-2">
-        {data.results.map((project) => (
+      <p className="mt-6 mb-3 text-xl font-bold sm:text-2xl">퍼블리싱 5개</p>
+      <ul className="max-w-screen-sm p-4 sm:max-w-screen-lg lg:w-3/4 grid grid-cols-1 gap-8 pb-8 lg:grid-cols-2">
+        {data.results.slice(0, 5).map((project) => (
+          <ProjectItem key={project.id} data={project} />
+        ))}
+      </ul>
+      <p className="mt-4 mb-3 text-xl font-bold sm:text-2xl">프론트엔드 4개</p>
+      <ul className="max-w-screen-sm p-4 sm:max-w-screen-lg lg:w-3/4 grid grid-cols-1 gap-8 pb-8 lg:grid-cols-2">
+        {data.results.slice(5).map((project) => (
           <ProjectItem key={project.id} data={project} />
         ))}
       </ul>
