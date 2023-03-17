9b2651f89333f65cd02a6e1b37a5595b00d840ad
Leechaehyeon <lch09253@gmail.com>
2023-03-17 23:50:52 +0900

    Change content

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -12,7 +12,7 @@ export default function ProjectList({ data }) {
         의 작업물입니다.
       </span>
       <strong className="mt-4 text-center break-keep dark:text-white">
-        Next.js 1개, React.js 4개, Javascript 4개, jQuery 1개
+        Next.js 1개, React.js 3개, Javascript 4개, jQuery 1개
       </strong>
       <p className="mt-6 mb-3 text-xl font-bold sm:text-2xl">퍼블리싱 5개</p>
       <ul className="max-w-screen-sm p-4 sm:max-w-screen-lg lg:w-3/4 grid grid-cols-1 gap-8 pb-8 lg:grid-cols-2">
@@ -20,7 +20,7 @@ export default function ProjectList({ data }) {
           <ProjectItem key={project.id} data={project} />
         ))}
       </ul>
-      <p className="mt-4 mb-3 text-xl font-bold sm:text-2xl">프론트엔드 5개</p>
+      <p className="mt-4 mb-3 text-xl font-bold sm:text-2xl">프론트엔드 4개</p>
       <ul className="max-w-screen-sm p-4 sm:max-w-screen-lg lg:w-3/4 grid grid-cols-1 gap-8 pb-8 lg:grid-cols-2">
         {data.results.slice(5).map((project) => (
           <ProjectItem key={project.id} data={project} />
